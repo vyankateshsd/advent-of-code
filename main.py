5699bc844bdd29b2227e03629b4f1c75247533d1
Vyankatesh Dande <vyankateshdande.vd@gmail.com>
2026-05-13 19:42:39 +0530

    Created a main script to solve both puzzles of a desired day

---
+++ main.py
@@ -0,0 +1,19 @@
+import importlib
+from get_input import get_input
+
+def solve(year: int, day: int):
+    module = importlib.import_module(f"{year}.day{day}")
+    part1, part2 = module.part1, module.part2
+
+    input = get_input(year, day)
+    print("Part 1:", part1(input))    
+    print("Part 2:", part2(input))
+
+def main():
+    year = int(input("Enter year: "))
+    day = int(input("Enter day: "))
+
+    solve(year, day)
+
+if __name__ == "__main__":
+    main()
