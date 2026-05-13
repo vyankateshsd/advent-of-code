99893d5b9020692025fadba13e301eae2cb7d0bc
Vyankatesh Dande <vyankateshdande.vd@gmail.com>
2026-05-13 19:41:38 +0530

    Created a function to get input

---
+++ get_input.py
@@ -0,0 +1,10 @@
+import requests
+from os import environ
+
+def get_input(year: int, day: int) -> str:
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
+    session_cookie = environ.get("AOC_COOKIE")
+    if session_cookie is None:
+        raise Exception("AOC_COOKIE environment variable not set")
+    response = requests.get(url, cookies={"session": session_cookie})
+    return response.text
