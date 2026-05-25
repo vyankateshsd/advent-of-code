0f1d3ffcc40142e63700db208b528ff7e93145d6
Vyankatesh Dande <vyankateshdande.vd@gmail.com>
2026-05-25 23:34:02 +0530

    Changed year and day from interactive input to CLI args in main.py

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
+import sys
 import importlib
 from dotenv import load_dotenv
+from datetime import datetime
 
 from get_input import get_input
 
 load_dotenv()
 
-def solve(year: int, day: int):
+def main():
+    if len(sys.argv) < 3:
+        raise Exception("Provide year and day")
+
+    year = int(sys.argv[1])
+    if year < 2015 or year > datetime.now().year:
+        raise Exception("Invalid year")
+    
+    day = int(sys.argv[2])
+    if day < 1 or day > 25:
+        raise Exception("Invalid date")
+
     module = importlib.import_module(f"{year}.day{day}")
     part1, part2 = module.part1, module.part2
 
     input = get_input(year, day)
-    print("Part 1:", part1(input))    
+    print("Part 1:", part1(input))
     print("Part 2:", part2(input))
-
-def main():
-    year = int(input("Enter year: "))
-    day = int(input("Enter day: "))
-
-    solve(year, day)
 
 if __name__ == "__main__":
     main()
